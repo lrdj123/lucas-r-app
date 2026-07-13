ca841f12d46c0f6ec329fb4450f145f250f2ac0f
lrdj123 <lucaslrdj1993@gmail.com>
2026-07-13 02:14:09 -0300

    Adiciona allow_unsafe_werkzeug

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ WSGI entry point para Back4App
 from app import app, socketio
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=8080)
+    socketio.run(app, host='0.0.0.0', port=8080, allow_unsafe_werkzeug=True)
